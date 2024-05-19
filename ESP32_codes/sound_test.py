4a88115d9b4b018560bcb0694df32e075580a923
Silvia Binda <79167212+silviabinda@users.noreply.github.com>
2024-05-19 12:29:44 +0200

    Update sound_test.py

--- ESP32_codes/sound_test.py
+++ ESP32_codes/sound_test.py
@@ -1,21 +1,22 @@
+# This is a code to simply test the functionality of the speaker and amplifier
+
 import machine
 import time
-import random # for random numbers for the frequency and duration of sounds
 from machine import Pin
 
-# Define speaker pin
+# Define the speaker pin
 SPEAKER_PIN = 25
 
 # Set up PWM for the speaker
 speaker = machine.PWM(machine.Pin(SPEAKER_PIN))
 
-# Initialize random seed
-random.seed(int(time.time()))
-
+# We create a function, we name it DataSonification
 def DataSonification():
-
     while True:
-        speaker.freq(10)
+        speaker.freq(100)
         speaker.duty(512)
+# info: speaker.freq determines the Frequency in Hz. Values from 0 up to 20000 (=20 kHz, which is the limit of audible range for humans)
+# info: speaker.duty determines the volume and characteristics of the sound. Values from 0 to 1023 (for a 10bit PWM resolution, which is default)
 
-DataSonification()
+# Run the function:
+DataSonification() 
